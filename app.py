d27240a216ccc7259ac3cc58524cb1640b24d116
Semen Kiselev <1semencs1@gmail.com>
2020-09-12 20:42:21 +0300

    send poly

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 #app.py
 
-from flask import Flask, request #import main Flask class and request object
-
-app = Flask(__name__) #create the Flask app
-farms = [1,2,3,4,5]
+from flask import Flask, request, jsonify
+import geopandas as gpd
 
 
-@app.route('/query-example')
-def query_example():
-    language = request.args.get('language')
-    return 'Todo... '+ str(language)
+app = Flask(__name__)
 
-@app.route('/form-example')
-def formexample():
-    return 'Todo...'
+df = gpd.read_file('data/Поля_Полигональные2.shp')
 
-@app.route('/get_farmer')
-def jsonexample():
-    idx = request.args.get('idx')
-    return f'Farmer is {farms[int(idx)]}'
+def read_poly(df):
+    row = df[df.ORIG_FID == 2049].copy()
+    poly = row.geometry
+    return poly.to_json()
+
+
+@app.route('/get_poly')
+def get_poly():
+    poly = read_poly(df)
+    return jsonify({
+        'geometries': [poly]
+    })
+
 
 @app.route('/')
 def index():
-    idx = 1
-    return f'Farmer is {farms[int(idx)]}'
+    return f'<a href="http://localhost:5000/get_poly">get poly</a>'
 
 if __name__ == '__main_':
     app.run(debug=True, port=5000)  #run app in debug mode on port 5000
