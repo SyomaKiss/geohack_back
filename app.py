456a48ef19921fc8b6583d4d9ece6a558ede336c
Semen Kiselev <1semencs1@gmail.com>
2020-09-13 13:03:49 +0300

    get poly by index

--- app.py
+++ app.py
@@ -8,22 +8,25 @@ app = Flask(__name__)
 
 path = 'data/perm_AOI/perm_AOI.shp'
 df = gpd.read_file(path)
+
 def read_poly(df, idx=2049):
-    row = df[df.ORIG_FID == idx].copy()
+    row = df[df.ORIG_FID == idx]
     poly = row.geometry
     return poly.to_json()
 
 
-@app.route('/get_poly')
-def get_poly():
-    poly = read_poly(df)
-    return jsonify({
-        'geometries': [poly]
-    })
+# @app.route('/get_poly')
+# def get_poly():
+#     poly = read_poly(df)
+#     return jsonify({
+#         'geometries': [poly]
+#     })
 
 @app.route('/api/get_poly')
 def get_poly2():
-    poly = read_poly(df)
+    idx = int(request.args.get('idx'))
+    print(idx)
+    poly = read_poly(df, idx)
     return jsonify({
         'geometries': [poly]
     })
@@ -31,7 +34,7 @@ def get_poly2():
 
 @app.route('/')
 def index():
-    return f'<a href="http://localhost:5000/get_poly">get poly</a>'
+    return f'<a href="http://localhost:5000/api/get_poly?idx=10">get poly</a>'
 
 if __name__ == '__main_':
     app.run(debug=True, port=5000)  #run app in debug mode on port 5000
